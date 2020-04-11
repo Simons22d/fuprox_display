ecea0118aa6fb399ae1171df1e328779210757b2
simons22d <denniskiruku@gmail.com>
2020-04-11 15:12:37 +0300

    init

---
+++ main.js
@@ -0,0 +1,53 @@
+const { app,BrowserWindow,ipcMain } = require('electron')
+const path=require('path')
+const url=require('url')
+
+let win;
+let token;
+
+
+const {globalShortcut } = require('electron')
+
+function createWindows() {
+    
+    win = new BrowserWindow({
+                height:700,
+                width: 1200, 
+                minHeight:700,
+                minWidth: 1200,
+                allowRendererProcessReuse : true
+    })
+
+    // win.webContents.openDevTools()
+
+    win.loadURL(url.format({
+        pathname:path.join(__dirname,'index.html'),
+        protocol:'file',
+        slashes:true,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    }))
+}
+
+
+// ipcMain.on('asynchronous-message', (event, arg) => {
+//   console.log(arg)
+//   let interval  = setInterval(()=>{
+//     event.reply('asynchronous-reply', users_token)
+//   },1000)
+  
+//    setTimeout(()=>{
+//       clearInterval(interval)
+//   },60000)
+// })
+
+
+app.on('ready',() =>{
+    createWindows()
+     globalShortcut.register('CommandOrControl+Y', () => {
+        // Do stuff when Y and either Command/Control is pressed.
+    })
+
+})
+
