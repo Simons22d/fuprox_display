3fc6d58bd556e44bfeffde232745de4d714c6ae4
simons22d <denniskiruku@gmail.com>
2020-07-20 20:55:48 +0300

    added fix and hide dev tools

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function createWindows() {
     // minimizable : false,
     resizable :false,
 
-    win.webContents.openDevTools()
+    // win.webContents.openDevTools()
 
     win.loadURL(url.format({
         pathname:path.join(__dirname,'index.html'),
